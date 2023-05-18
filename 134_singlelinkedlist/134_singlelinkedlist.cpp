317d475fd28238ebbe8361603b8fbcaa1fb90c77
RidwanC134 <ridwanhh10@gmail.com>
2023-05-18 22:55:04 +0700

    Membuat int nim case 3, 4, 5, dan default. Membuat catch exception e untuk mengatasi dan menampilkan kesalahan yang dihasilkan

--- 134_singlelinkedlist/134_singlelinkedlist.cpp
+++ 134_singlelinkedlist/134_singlelinkedlist.cpp
@@ -168,8 +168,22 @@ int main() {
 		system("pause");
 		system("cls");
 	}
-
+	else
+		cout << "Data tidak ditemukan" << endl;
+	break;
+			case 3:
+				traverse();
+				break;
+			case 4:
+				searchData();
+				break;
+			case 5:
+				break;
+			default:
+				cout << "Pilihan tidak ada" << endl;
+				break;
 			}
 		}
+		catch (exception e)
 	}
 }
